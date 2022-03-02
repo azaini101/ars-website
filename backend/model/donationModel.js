4c955eb4a982a939ae6c30380a7d640d97999fdb
zaini <alimzaini@outlook.com>
2022-03-02 10:10:58 +0000

    Minor backend refactor

--- backend/model/donationModel.js
+++ backend/model/donationModel.js
@@ -1,16 +1,16 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
 const donationsSchema = {
-    "firstName": String,
-    "lastName": String,
-    "email": String,
-    "phone": String,
-    "emergencyfirstName": String,
-    "emergencylastName": String,
-    "emergencyphone": String,
-    "service": String,
-    "description": String
-}
+  firstName: String,
+  lastName: String,
+  email: String,
+  phone: String,
+  emergencyFirstName: String,
+  emergencyLastName: String,
+  emergencyPhone: String,
+  service: String,
+  description: String,
+};
 
 const Donation = mongoose.model("Donation", donationsSchema);
 
--- backend/server.js
+++ backend/server.js
@@ -1,28 +1,30 @@
-require('dotenv').config()
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
-const app = express();
-const router = express.Router();
-const Donation = require("./model/donationModel")
+const Donation = require("./model/donationModel");
+
 const PORT = process.env.PORT || 5000;
+
+const app = express();
+
 app.use(cors());
 app.use(express.json());
-mongoose.connect(`mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@cluster0.sv3hk.mongodb.net/${process.env.DBNAME}`);
-mongoose.connection.on('connected', () => {
-  console.log("connected");
+
+mongoose.connect(process.env.MONGODB_URI);
+
+mongoose.connection.on("connected", () => {
+  console.log("MongoDB db connection established");
 });
 
-app.post('/submitForm', function (req, res) {
+app.post("/submitForm", (req, res) => {
+  console.log("/submitForm");
   var donation = new Donation(req.body);
-  donation.save()
-    .then(data => {
-      console.log("Data has been saved.")
-      res.send("Data saved to db.");
-    });
-  console.log(req.body)
-})
+  donation.save().then((data) => {
+    res.send(data);
+  });
+});
 
-app.listen(PORT, function () {
-   console.log(`Example app listening ON ${PORT}`)
-})
+app.listen(PORT, () => {
+  console.log(`Backend listening on http://localhost:${PORT}`);
+});
