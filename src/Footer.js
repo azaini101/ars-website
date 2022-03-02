4b37b39b79963d55e8b0d2aeaf2b2e8230b0bd4c
zaini <alimzaini@outlook.com>
2022-03-02 10:44:04 +0000

    Refactor frontend and add footer

---
+++ src/Footer.js
@@ -0,0 +1,14 @@
+import { Center, Container, Text } from "@chakra-ui/react";
+import React from "react";
+
+const Footer = () => {
+  return (
+    <Text textAlign={"center"} fontSize={18} pt={6}>
+      Idara Jaferia and Afghan SIV Support &copy; 2022 |{" "}
+      <a href="mailto:contact@email.com">contact@email.com</a> |{" "}
+      <a href="tel:+1 555 555 5555">+1 555 555 5555</a>
+    </Text>
+  );
+};
+
+export default Footer;
