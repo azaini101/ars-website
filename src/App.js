dc650abab1a515275c35db304b332561ec82adc1
zaini <alimzaini@outlook.com>
2022-03-02 10:44:04 +0000

    Refactor frontend and add footer

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import React from "react";
+import { ChakraProvider, CSSReset, extendTheme } from "@chakra-ui/react";
 import HookForm from "./HookForm";
-import { DarkMode, GlobalStyle, ChakraProvider, CSSReset, Box } from "@chakra-ui/react";
+import Footer from "./Footer";
+
+const config = {
+  initialColorMode: "dark",
+  useSystemColorMode: false,
+};
+
+const theme = extendTheme({ config });
 
 export default function App() {
   return (
-    <ChakraProvider>
-      <DarkMode>
-        <GlobalStyle />
-        <CSSReset />
-        <Box p={4}>
-          <HookForm />
-        </Box>
-      </DarkMode>
+    <ChakraProvider theme={theme}>
+      <CSSReset />
+      <HookForm />
+      <Footer />
     </ChakraProvider>
   );
 }
--- src/HookForm.js
+++ src/HookForm.js
@@ -7,87 +7,128 @@ import {
   Button,
   Container,
   Heading,
-  Wrap,
   Stack,
   Textarea,
-  Box
 } from "@chakra-ui/react";
 import { Select } from "chakra-react-select";
 import { serviceOptions } from "./docs/data";
 
 export default function HookForm() {
-  const { register, handleSubmit, formState: { errors, isSubmitting } } = useForm();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+  } = useForm();
+
   const onSubmit = async (data) => {
-    console.log(data)
-    const response = await fetch("https://vast-woodland-44303.herokuapp.com/submitForm/", {
-      method: 'POST', // *GET, POST, PUT, DELETE, etc
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify(data)
-    });
-    console.log(response)
-    window.alert("Form has been submitted")
+    const response = await fetch(
+      "https://vast-woodland-44303.herokuapp.com/submitForm/",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      }
+    );
+    // TODO check for errors
+    window.alert("Form has been submitted");
     window.location.reload(false);
   };
+
+  // TODO show an actual error message
   console.log(errors);
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
-      <Container maxW='container.md'>
-        <Box boxShadow='dark-lg' p='8'>
-          <Wrap spacing='30px' align='center'>
-            <Heading>
-              Idara Jaferia and Afghan SIV Support
-            </Heading>
-            <FormControl isRequired>
-              <Stack direction={['column']} spacing='24px'>
-                <Heading as='h3' size='md'>Volunteer Information</Heading>
-                <Box boxShadow='outline' p='5'>
-                  <FormLabel htmlFor="firstName">First Name</FormLabel>
-                  <Input id="firstName" placeholder="First Name" {...register("firstName")} />
+      <Container maxW={"container.md"} boxShadow={"dark-lg"} p={8}>
+        <Heading>Idara Jaferia and Afghan SIV Support</Heading>
 
-                  <FormLabel htmlFor="lastName">Last Name</FormLabel>
-                  <Input id="lastName" placeholder="Last Name" {...register("lastName")} />
+        <br />
 
-                  <FormLabel htmlFor='email'>Email Address</FormLabel>
-                  <Input id='email' type='email' placeholder="Email Address" {...register("email")} />
+        <FormControl isRequired>
+          <Stack spacing={6}>
+            <Stack>
+              <Heading size="md">Volunteer Information</Heading>
 
-                  <FormLabel htmlFor='phone'>Phone Number</FormLabel>
-                  <Input type="number" placeholder="2021234567" {...register("phone")} />
-                </Box>
+              <FormLabel htmlFor="firstName">First Name</FormLabel>
+              <Input
+                id="firstName"
+                placeholder="First Name"
+                {...register("firstName")}
+              />
 
+              <FormLabel htmlFor="lastName">Last Name</FormLabel>
+              <Input
+                id="lastName"
+                placeholder="Last Name"
+                {...register("lastName")}
+              />
 
-                <Heading as='h3' size='md'>Emergency Contact Information</Heading>
-                <Box boxShadow='outline' p='5'>
-                  <FormLabel htmlFor="emergencyfirstName">First Name</FormLabel>
-                  <Input id="emergencyfirstName" placeholder="First Name" {...register("emergencyfirstName")} />
+              <FormLabel htmlFor="email">Email Address</FormLabel>
+              <Input
+                id="email"
+                type="email"
+                placeholder="email@address.com"
+                {...register("email")}
+              />
 
-                  <FormLabel htmlFor="emergencylastName">Last Name</FormLabel>
-                  <Input id="emergencylastName" placeholder="Last Name" {...register("emergencylastName")} />
+              <FormLabel htmlFor="phone">Phone Number</FormLabel>
+              <Input placeholder="+1 555 555 5555" {...register("phone")} />
+            </Stack>
 
-                  <FormLabel htmlFor='emergencyphone'>Phone Number</FormLabel>
-                  <Input type="number" placeholder="2021234567" {...register("emergencyphone")} />
-                </Box>
+            <Stack>
+              <Heading size="md">Emergency Contact Information</Heading>
 
-                <FormLabel htmlFor='service'>Service</FormLabel>
-                <Select
-                  isMulti
-                  placeholder='Select services'
-                  options={serviceOptions}
-                  closeMenuOnSelect={false}
-                  {...register("service")}>
-                </Select>
+              <FormLabel htmlFor="emergencyFirstName">First Name</FormLabel>
+              <Input
+                id="emergencyFirstName"
+                placeholder="First Name"
+                {...register("emergencyFirstName")}
+              />
 
-                <FormLabel htmlFor='description'>Please enter what you can provide below.</FormLabel>
-                <Textarea placeholder='We thank you in advance for your contribution!' {...register("description")} />
-              </Stack>
-            </FormControl>
-            <Button colorScheme="teal" isLoading={isSubmitting} type="submit">
-              Submit
-            </Button>
-          </Wrap>
-        </Box>
+              <FormLabel htmlFor="emergencyLastName">Last Name</FormLabel>
+              <Input
+                id="emergencyLastName"
+                placeholder="Last Name"
+                {...register("emergencyLastName")}
+              />
+
+              <FormLabel htmlFor="emergencyPhone">
+                Emergency Phone Number
+              </FormLabel>
+              <Input
+                placeholder="+1 555 555 5555"
+                {...register("emergencyPhone")}
+              />
+            </Stack>
+
+            <Stack>
+              <FormLabel htmlFor="service">Service(s)</FormLabel>
+              <Select
+                isMulti
+                placeholder="Select services"
+                options={serviceOptions}
+                closeMenuOnSelect={false}
+                {...register("service")}
+              ></Select>
+
+              <FormLabel htmlFor="description">
+                Provide more information about your services
+              </FormLabel>
+              <Textarea
+                placeholder="We thank you in advance for your contribution!"
+                {...register("description")}
+              />
+            </Stack>
+          </Stack>
+        </FormControl>
+
+        <br />
+
+        <Button colorScheme="teal" isLoading={isSubmitting} type="submit">
+          Submit
+        </Button>
       </Container>
     </form>
   );
