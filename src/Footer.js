0c7d217c822440ccc05fddf0c200097081edcc8d
Ali Zaini <azaini@uno.edu>
2022-03-22 13:52:56 -0500

    change email

--- src/Footer.js
+++ src/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <Text textAlign={"center"} fontSize={18} pt={6}>
       Idara-e-Jaferia Refugee Support &copy; 2022 |{" "}
-      <a href="mailto:contact@email.com">hussain_jafri@hotmail.com</a> |{" "}
+      <a href="mailto:hussain@dmvrefugees.com">hussain@dmvrefugees.com</a> |{" "}
       <a href="tel:+1 443-717-2136">+1 443-717-2136</a>
     </Text>
   );
